0c6ea121578f09c49859fbc231ed68cffc0e7cf8
Paul <paul.laihonen@eficode.com>
2017-11-04 13:40:28 +0200

    Beautifyed stuff

--- src/content/info.js
+++ src/content/info.js
@@ -1,14 +1,14 @@
 export default `
 # Lisätietoa
 
-## Etiketti
+### Etiketti
 Killan kymmenvuotisjuhliin kasattiin erinomainen rautaisannos Stimulaatioetiketistä, pukeutumisesta, käytöstavoista ja juhlan kulusta. Koonnut Riikka Liedes.  
 [Vuosijuhlaetiketti - Muistia virkistämään Stimulaatioon 2008](/etiketti2008.pdf) [PDF, 267KB]
 
 Edesmenneen TKY:n etikettioppaasta löytyy edellistä laajempi oppimäärä juhlaetikettiin ja kaikkeen vuosijuhliin liittyvään. Kirjoittanut Tiina Metso.  
 [Tavoistaan teekkari tunnetaan](/etiketti_tky.pdf) [PDF, 1.25MB]
 
-## Yhteystiedot
+### Yhteystiedot
 Juhlia koskeviin kysymyksiin vastaa Stimulantti
 
 Paul Laihonen  
