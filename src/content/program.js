c0c462b8cebc2980b047beb00944089c1945e65b
Sanna Halmkrona <halmkrs1@aalto.fi>
2019-09-04 17:42:58 +0300

    Updated general info

--- src/content/program.js
+++ src/content/program.js
@@ -1,7 +1,7 @@
 export default `
 # Ohjelma
 
-### Perjantai 15.12.
+### Perjantai 15.11.
 
 **16.00** - Cocktailtilaisuus,  TUAS-talo ([Maarintie 8, 02150 Espoo](https://www.google.fi/maps/place/Maarintie+8,+02150+Espoo/@60.1867263,24.8166308,17z/data=!3m1!4b1!4m5!3m4!1s0x468df5eb2f84cc95:0x1f13f2fbc064e8d!8m2!3d60.1867263!4d24.8188195?hl=fi))
 
@@ -9,7 +9,7 @@ export default `
 
 Jatkot, Paikka X (kuljetukset pääjuhlasta)
 
-### Lauantai 16.12.
+### Lauantai 16.11.
 
 **12.00** - Stillis, Rantasauna ([Vastaranta 1, 02150 Espoo](https://www.google.fi/maps/place/Vastaranta+1,+02150+Espoo/@60.1880714,24.836629,17z/data=!3m1!4b1!4m5!3m4!1s0x468df5f21cae6455:0x6be92ebfff50eeb4!8m2!3d60.1880714!4d24.8388177?hl=fi))
 `;
