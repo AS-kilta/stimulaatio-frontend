e9932794645e0905d32201867a89256a3c65cb71
Michael Casserly <michael.casserly@aalto.fi>
2020-08-09 13:20:44 +0300

    New payment information

--- src/content/registration/payment-information.js
+++ src/content/registration/payment-information.js
@@ -5,5 +5,5 @@ Saaja: Automaatio- ja systeemitekniikan kilta ry
 IBAN: FI84 3131 3001 8081 61  
 Viite: 25881  
 Hinta: 70 € (opiskelija) tai 85 € (valmistunut), Stillis 25 €  
-Eräpäivä: 15.10.2019
+Eräpäivä: 13.10.2020
 `;
