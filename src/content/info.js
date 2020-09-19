e6d5c6f0646e6e823c929c5b50255604d8ccfcbb
pulmari <mari.h.pulkkinen@gmail.com>
2020-09-19 19:01:37 +0300

    Etiquette updated

--- src/content/info.js
+++ src/content/info.js
@@ -1,17 +1,43 @@
 export default `
 # Lisätietoa
 
-### Etiketti
-Killan kymmenvuotisjuhliin kasattiin erinomainen rautaisannos Stimulaatioetiketistä, pukeutumisesta, käytöstavoista ja juhlan kulusta. Koonnut Riikka Liedes.
-[Vuosijuhlaetiketti - Muistia virkistämään Stimulaatioon 2008](/etiketti2008.pdf) (PDF, 267KB)
-
-Edesmenneen TKY:n etikettioppaasta löytyy edellistä laajempi oppimäärä juhlaetikettiin ja kaikkeen vuosijuhliin liittyvään. Kirjoittanut Tiina Metso.
-[Tavoistaan teekkari tunnetaan](/etiketti_tky.pdf) (PDF, 1.25MB)
-
 ### Yhteystiedot
 Juhlia koskeviin kysymyksiin vastaa Stimulantti
 
 Mari Pulkkinen  
 stimulantti(at)as.fi  
 [@maripulkkinen](https://telegram.me/maripulkkinen) 
+
+### Etiketti
+##### Käytös
+
+Vuosijuhlat ovat arvokas pöytäjuhla, jossa vierailta odotetaan hyviä pöytätapoja ja muiden vieraiden kunnioittamista. Ethän siis huutele kovaan ääneen, käytä puhelintasi koko aikaa ja kuuntelethan puheet ja mahdolliset esitykset keskeyttämättä ja häiritsemättä. 
+
+Laulujen ja puheiden aikana on myös kohteliasta olla syömättä ja juomatta. Vuosijuhlat eroavat teekkarisitseistä myös siten, että tilaisuuden aikana pidetään sitsitauko, jonka aikana voit käydä vierailemassa muissa pöydissä, puuteroimassa nenäsi tai vaikka suorittaa myöhäisillan bisnespuhelun. Kohteliasta on myös olla sammumatta pöytään juhlan aikana.
+
+##### Juhlan kulku
+
+Cocktail-tilaisuudessa eri kiltojen ja muiden yhteisöjen edustajat voivat antaa hallitukselle lahjan. Lahjan antajaksi ilmoittaudutaan jo itse juhlaan ilmoittautuessa ja lahjananto koskee lähinnä kutsuvieraita. Cocktail-tilaisuudesta siirrytään pääjuhlapaikalle yleensä bussikuljetuksella.
+
+Pääjuhlassa jokaiselle on oma paikkansa ja paikalta ei tule poistua juhlan aikana muuta kuin aivan välttämättömissä tapauksissa. On kohteliasta esittäytyä vieressä istuville henkilöille, jos et heitä tunne. Skoolausjärjestys on sama kuin sitseillä, joka toinen aloittaa oikealta ja joka toinen vasemmalta.
+
+Vuosijuhlilla kuullaan muutamia puheita, ja niiden järjestys on yleensä esitetty pöydissä sijaitsevissa pöytäohjelmissa. Seremoniamestari ilmoittaa aina, kun jokin puhe tai muu tärkeä asia on alkamassa, jolloin on syytä hiljentyä kuuntelemaan.
+
+Vuosijuhlien ei ole tarkoitus olla pelkkää pönötystä ja juuri maalaisjärjellä ja hyviä käytöstapoja noudattamalla pärjää pitkälle! Vuosijuhlat ovat kuitenkin arvokas tapahtuma ja siksi koitetaan parhaamme mukaan kunnioittaa tapahtuman luonnetta.
+
+##### DC
+
+Pukukoodi vuosijuhlilla on yleensä juhlapuku, ja tämä voi tarkoittaa siis frakkia tai iltapukua. Pukukoodin voi tulkita itselle sopivalla tavalla, kunhan se on juhlava. Esimerkiksi frakin sijaan värikäs puku ei ole juhlava, mutta tumma puku on. Jos haluat pukea mekon, yleinen ohje iltapuvulle on pitkä. Pukukoodi ei kuitenkaan ole kiveen hakattu!
+
+##### Kunniamerkit ja nauhat
+
+Eri järjestöillä ja killoilla voi olla omia kunnia-/ansiomerkkejään ja/tai nauhojaan. Kunniamerkillä tai nauhalla voi olla jokin oma käyttöohjesääntönsä, jonka todennäköisesti itse tiedät kun sellaisen saat.
+
+Yleisiä ohjeita nauhoille on, että kantajalla olisi hyvä olla rajoitettu nauhojen määrä maksimissaan kolmeen erilaiseen ja nauha ei saa koskettaa paljasta ihoa. Nauhan voi myös itse taiteilla rusetiksi, joka onkin iltapukujen kanssa yleensä helpompi ratkaisu. Jos sinulla on useampi nauha, joita kuuluu käyttää ylimpänä, et voi käyttää niitä samanaikaisesti. Nauhaa myös pidetään aina oikealta olkapäältä vasemmalle lantiolle.
 `;
+
+/* Killan kymmenvuotisjuhliin kasattiin erinomainen rautaisannos Stimulaatioetiketistä, pukeutumisesta, käytöstavoista ja juhlan kulusta. Koonnut Riikka Liedes.
+[Vuosijuhlaetiketti - Muistia virkistämään Stimulaatioon 2008](/etiketti2008.pdf) (PDF, 267KB)
+
+Edesmenneen TKY:n etikettioppaasta löytyy edellistä laajempi oppimäärä juhlaetikettiin ja kaikkeen vuosijuhliin liittyvään. Kirjoittanut Tiina Metso.
+[Tavoistaan teekkari tunnetaan](/etiketti_tky.pdf) (PDF, 1.25MB) */
