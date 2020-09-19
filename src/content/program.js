51d3ad5f18f886c92b57160f5cdec9e6aaad63c9
pulmari <mari.h.pulkkinen@gmail.com>
2020-09-19 20:30:22 +0300

    Texts updated

--- src/content/program.js
+++ src/content/program.js
@@ -3,13 +3,9 @@ export default `
 
 ### Perjantai 13.11.
 
-**16.00** - Cocktailtilaisuus,  TUAS-talo ([Maarintie 8, 02150 Espoo](https://www.google.fi/maps/place/Maarintie+8,+02150+Espoo/@60.1867263,24.8166308,17z/data=!3m1!4b1!4m5!3m4!1s0x468df5eb2f84cc95:0x1f13f2fbc064e8d!8m2!3d60.1867263!4d24.8188195?hl=fi))
+**17.30** - Cocktailtilaisuus,  Opinmäki ([Lillhemtintie 1, 02250 Espoo](https://goo.gl/maps/MS7DcJPW9y3dFKPs9))
 
 **19.00** - Pääjuhla, Opinmäki ([Lillhemtintie 1, 02250 Espoo](https://goo.gl/maps/MS7DcJPW9y3dFKPs9))
 
-Jatkot, Paikka X (kuljetukset pääjuhlasta)
-
-### Lauantai 14.11.
-
-**12.00** - Stillis, Rantasauna ([Vastaranta 1, 02150 Espoo](https://www.google.fi/maps/place/Vastaranta+1,+02150+Espoo/@60.1880714,24.836629,17z/data=!3m1!4b1!4m5!3m4!1s0x468df5f21cae6455:0x6be92ebfff50eeb4!8m2!3d60.1880714!4d24.8388177?hl=fi))
+Mahdollisista jatkoista ja seuraavan päivän Stilliksestä tiedotetaan lähempänä tapahtumaa.
 `;
--- src/content/registration/registration-main.js
+++ src/content/registration/registration-main.js
@@ -1,8 +1,8 @@
 export default `
-**29.9.2020 klo 18** - Ilmoittautuminen aukeaa kutsuvieraille.
+**6.10.2020 klo 18** - Ilmoittautuminen aukeaa kutsuvieraille.
 
-**6.10.2020 klo 18** - Ilmoittautuminen aukeaa kaikille vieraille.
+**13.10.2020 klo 18** - Ilmoittautuminen aukeaa kaikille vieraille.
 
-**13.10.2020** - Ilmoittautuminen sulkeutuu.
+**20.10.2020** - Ilmoittautuminen sulkeutuu.
 
 `;
