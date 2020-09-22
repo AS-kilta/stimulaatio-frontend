1139418adc933bc280f85e43b4e2892e26eb5643
pulmari <mari.h.pulkkinen@gmail.com>
2020-09-22 18:39:34 +0300

    Hide registration

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Container, Navbar } from 'reactstrap';
+import { Container } from 'reactstrap';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -7,25 +7,14 @@ import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import '../styles/index.css';
 import '../components/NavigationTo.js';
 
-import { RegistrationMain, RegistrationForm, ParticipantList, PaymentInformation } from '.';
-import NavigationTo from '../components/NavigationTo.js';
+import { RegistrationMain } from '.';
 
 export function Registration() {
   return (
     <Router>
       <div>
-        <Navbar id="registration-navbar" color="faded" light expand="sm">
-          <Nav navbar>
-            <NavigationTo title="Ilmoittaudu" path="/ilmoittautuminen/ilmoittaudu" />
-            <NavigationTo title="Ilmoittautuneet" path="/ilmoittautuminen/ilmoittautuneet" />
-            <NavigationTo title="Maksutiedot" path="/ilmoittautuminen/maksutiedot" />
-          </Nav>
-        </Navbar>
         <Container>
           <Route exact path="/ilmoittautuminen" component={RegistrationMain} />
-          <Route path="/ilmoittautuminen/ilmoittaudu" component={RegistrationForm} />
-          <Route path="/ilmoittautuminen/ilmoittautuneet" component={ParticipantList} />
-          <Route path="/ilmoittautuminen/maksutiedot" component={PaymentInformation} />
         </Container>
       </div>
     </Router>
